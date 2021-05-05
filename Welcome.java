594f5bbe697bd82cc143438db5861e3d9ed5f9a3
Yaswanth Pantagani <83690170+Yaswanth-7774@users.noreply.github.com>
2021-05-05 13:07:42 +0530

    Add files via upload
    this is first commit

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class Welcome {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hii this is Yaswanth.This is my First Git Project");
+		
+	}
+
+}
